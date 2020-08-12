5ec3f7169a572676310d5d8d7719920bd9417d35
Jessica Cho <jessicacho2013@gmail.com>
2020-08-11 21:45:53 -0700

    Inserted texts using innerText properties

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const title = document.getElementById("FortuneTitle");
+title.innerHTML = "Fortune Cookie";
+
+const subTitle = document.getElementById("FortuneSubtitle");
+subTitle.innerHTML = "What is today's horoscope?";
