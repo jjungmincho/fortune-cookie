ed277ab9023cde1d6aa91a83c3b58f5fbeb26389
Jessica Cho <jessicacho2013@gmail.com>
2020-09-08 19:50:16 -0700

    Added to toggle class in JS

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ const title = document.getElementById("FortuneTitle");
 title.innerHTML = "Fortune Cookie";
 
 const subTitle = document.getElementById("FortuneSubtitle");
-subTitle.innerHTML = "What is today's horoscope?";
+subTitle.innerHTML = "What do you want to know?";
+
+function fortuneImg() {
+  let x = document.getElementById("paraBg");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
