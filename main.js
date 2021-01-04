7df9679a73698490ab818e3ed708837785c67ef3
Jessica Cho <jessicacho2013@gmail.com>
2021-01-03 20:48:40 -0800

    Fix image fuction and add quote function

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-"use strict";
 
-const title = (document.getElementById("FortuneTitle").innerHTML =
-  "Fortune Cookie");
+const title = (document.getElementById("FortuneTitle").innerHTML = "Fortune Cookie");
 
-const subTitle = (document.getElementById("FortuneSubtitle").innerHTML =
-  "What do you want to know?");
+const subTitle = (document.getElementById("FortuneSubtitle").innerHTML = "What do you want to know?");
 
 function getImg() {
   let fortuneImg = document.getElementById("paraBg");
@@ -14,3 +11,9 @@ function getImg() {
     fortuneImg.style.display = "none";
   }
 }
+
+getImg();
+
+const text = [`Today it's up to you to create the peacefulness you long for.`, `A friend asks only for your time not your money.`, `If you refuse to accept anything but the best, you very often get it.`, `A smile is your passport into the hearts of others.`];
+const randomText = Math.floor(Math.random() * text.length);
+document.getElementById("paraText").innerHTML = text[randomText];
